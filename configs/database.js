bb7f1201f85dd708ff53b8ff735b29289853c7d2
Muhammad Rafael Ghifari <business.rafaelghifari@gmail.com>
2023-11-30 14:22:42 +0700

    Update database.js

--- configs/database.js
+++ configs/database.js
@@ -5,34 +5,39 @@ import { Sequelize } from "sequelize";
 const env = process.env.ENV ?? "Development";
 
 const developmentConfig = {
-    host: process.env.DEV_HOST,
-    port: process.env.DEV_PORT,
-    database: process.env.DEV_DATABASE,
-    username: process.env.DEV_USERNAME,
-    password: process.env.DEV_PASSWORD
-}
+  host: process.env.DEV_HOST,
+  port: process.env.DEV_PORT,
+  database: process.env.DEV_DATABASE,
+  username: process.env.DEV_USERNAME,
+  password: process.env.DEV_PASSWORD,
+};
 
 const productionConfig = {
-    host: process.env.PROD_HOST,
-    port: process.env.PROD_PORT,
-    database: process.env.PROD_DATABASE,
-    username: process.env.PROD_USERNAME,
-    password: process.env.PROD_PASSWORD
-}
+  host: process.env.PROD_HOST,
+  port: process.env.PROD_PORT,
+  database: process.env.PROD_DATABASE,
+  username: process.env.PROD_USERNAME,
+  password: process.env.PROD_PASSWORD,
+};
 
 const config = env === "Development" ? developmentConfig : productionConfig;
 
-const connectionDatabase = new Sequelize(config.database, config.username, config.password, {
+const connectionDatabase = new Sequelize(
+  config.database,
+  config.username,
+  config.password,
+  {
     host: config.host,
     port: config.port,
     dialect: "mysql",
     timezone: "+07:00",
-});
+  }
+);
 
 try {
-    console.log("Connection has been established successfully.");
+  console.log("Connection has been established successfully.");
 } catch (error) {
-    console.error("Unable to connect to the database:", error);
+  console.error("Unable to connect to the database:", error);
 }
 
 export default connectionDatabase;
